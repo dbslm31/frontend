aa9fdc08edf4066fe1b5704067eb82921a55d511
dbslm31 <104209863+dbslm31@users.noreply.github.com>
2023-06-19 22:26:07 +0200

    fetch-api-and-handlesearch-function

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,40 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const SearchBar = () => {
-  const [searchQuery, setSearchQuery] = useState("");
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-  const handleSearchSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    try {
+      const response = await axios.get("http://localhost:5000/recipes/all", {
+        query,
+      });
+      setSearchResults(response.data.results);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <div>
-      <form onSubmit={handleSearchSubmit}>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={query}
-          onChange={(event) => setSearchQuery(event.target.value)}
-          placeholder="Rechercher..."
+          onChange={(event) => setQuery(event.target.value)}
         />
-        <button type="submit">Rechercher</button>
+        <button type="submit">Search</button>
       </form>
+      <div>
+        {searchResults.map((result) => (
+          <div key={result.id}>
+            <img src={result.image} alt={result.title} />
+            <h3>{result.title}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
